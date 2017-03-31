7b114ecc10384fd8c89ccebd927492fbd40297c3
Elliott Armstrong <Kandy-Man@users.noreply.github.com>
2017-03-31 22:22:12 +0100

    Created a Scriptable Object that will hold different word databases.  Created a custom editor for editing database.  Also formated Unity files to text instead of binary for easier VC.

---
+++ Assets/Scripts/Editor/WordDatabaseEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WordDatabase))]
+public class WordDatabaseEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        WordDatabase myTarget = (WordDatabase)target;
+
+        if (myTarget.Count > 0)
+        {
+            for (int i = 0; i < myTarget.Count; i++)
+            {
+                EditorGUILayout.BeginVertical("Box");
+                EditorGUILayout.BeginHorizontal();
+
+                myTarget.Set(EditorGUILayout.TextField(myTarget.AtIndex(i)), i);
+
+                if (GUILayout.Button("X", GUILayout.Width(25f)))
+                {
+                    myTarget.Delete(i);
+                }
+
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.EndVertical();
+            }
+        }
+
+        if (GUILayout.Button("Add new word"))
+        {
+            myTarget.AddWord();
+        }
+    }
+}
---
+++ Assets/Scripts/WordDatabase.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Word Database", menuName = "Word Database")]
+public class WordDatabase : ScriptableObject
+{
+    [SerializeField]
+    List<string> words = new List<string>();
+
+    public void AddWord()
+    {
+        words.Add("New Word...");
+    }
+
+    public int Count
+    {
+        get { return words.Count; }
+    }
+
+    public string AtIndex(int i)
+    {
+        return words[i];
+    }
+
+    public void Set(string word, int index)
+    {
+        words[index] = word;
+    }
+
+    public void Delete(int index)
+    {
+        words.RemoveAt(index);
+    }
+}
