8a615317d62698490d972e2a7a2c221c79f23b96
Elliott Armstrong <KandyMan1990@users.noreply.github.com>
2022-09-15 17:18:33 +0100

    Tweaks to high scores in prep for database scores

--- Assets/Scripts/HighScoreScene.cs
+++ Assets/Scripts/HighScoreScene.cs
@@ -5,48 +5,32 @@ using System.Collections.Generic;
 public class HighScoreScene : MonoBehaviour
 {
     [SerializeField] Text Title;
-    [SerializeField] Text Score1;
-    [SerializeField] Text Score2;
-    [SerializeField] Text Score3;
-    [SerializeField] Text Score4;
-    [SerializeField] Text Score5;
+    [SerializeField] GameObject ScorePrefab;
+    [SerializeField] Transform Panel;
 
     List<int> list;
 
     void Start()
     {
-        Load();
+        Title.text = $"{GameManager.Instance.DatabaseType} High Scores";
+        LoadScores();
     }
 
-    void Load()
+    void LoadScores()
     {
-        Title.text = GameManager.Instance.DatabaseType + " High Scores";
-
         list = HighScores.LoadScores(GameManager.Instance.ActiveFilename);
 
-        if (list != null)
+        if (list == null || list.Count == 0)
         {
-            if (list.Count > 0)
-            {
-                list.Sort();
-                list.Reverse();
-                if (list.Count > 5)
-                    while (list.Count > 5)
-                        list.RemoveAt(list.Count - 1);
+            var textObj = Instantiate(ScorePrefab, Panel).GetComponent<Text>();
+            textObj.text = "Failed to load high scores";
+            return;
+        }
 
-                try
-                {
-                    Score1.text = "1. " + list[0];
-                    Score2.text = "2. " + list[1];
-                    Score3.text = "3. " + list[2];
-                    Score4.text = "4. " + list[3];
-                    Score5.text = "5. " + list[4];
-                }
-                catch
-                {
-
-                }
-            }
+        for (var i = 0; i < list.Count; i++)
+        {
+            var score = Instantiate(ScorePrefab, Panel).GetComponent<Text>();
+            score.text = $"{i + 1}. username - {list[i]}";
         }
     }
 }
